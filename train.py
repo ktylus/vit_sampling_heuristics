0a0d5b0156808578b6177752a1e7550fe710ec49
ktylus <karaxek23@gmail.com>
2023-11-23 19:14:53 +0100

    small style fixes

--- train.py
+++ train.py
@@ -4,19 +4,19 @@ from src.basic_model import BasicViT
 from src.utils import train_with_params, load_and_transform
 
 parser = argparse.ArgumentParser(add_help=True, description="Python script to run Basic ViT model training")
-parser.add_argument('-f', metavar='f', type = float, help='fraction of the training set', default = 0.8)
-parser.add_argument('-lr', metavar='lr', type = float, help='learning rate', default = 0.001)
-parser.add_argument('-b_s', metavar='b_s', type = int , help='batch size', default = 32)
-parser.add_argument('-e_n', metavar='e_n', type = int, help='number of training epochs', default = 1)
-parser.add_argument('-u', metavar='u', type = bool , help='unfreeze weights for one epoch', default = False)
-parser.add_argument('-a_b', metavar='a_b', type = bool, help='train with unfreezed weights during the first epoch', default = False)
-parser.add_argument('-b', metavar='b', type = str , help='path to a file where base weights will be saved', default = "BASIC_MODEL.pt")
-parser.add_argument('-t', metavar='t', type = str, help='path to a file where training results should be saved', default = "TRAINED_MODEL.pt")
+parser.add_argument('-f', metavar='f', type=float, help='fraction of the training set', default=0.8)
+parser.add_argument('-lr', metavar='lr', type=float, help='learning rate', default=0.001)
+parser.add_argument('-b_s', metavar='b_s', type=int, help='batch size', default=32)
+parser.add_argument('-e_n', metavar='e_n', type=int, help='number of training epochs', default=1)
+parser.add_argument('-u', metavar='u', type=bool, help='unfreeze weights for one epoch', default=False)
+parser.add_argument('-a_b', metavar='a_b', type=bool, help='train with unfreezed weights during the first epoch', default=False)
+parser.add_argument('-b', metavar='b', type=str, help='path to a file where base weights will be saved', default="BASIC_MODEL.pt")
+parser.add_argument('-t', metavar='t', type=str, help='path to a file where training results should be saved', default="TRAINED_MODEL.pt")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    datasets = load_and_transform(f = args.f)
+    datasets = load_and_transform(f=args.f)
 
     basic_model = BasicViT()
     torch.save(basic_model, args.b)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     }
     criterion = torch.nn.CrossEntropyLoss()
 
-    accuracy, model = train_with_params(params,criterion, datasets, **specs)
+    accuracy, model = train_with_params(params, criterion, datasets, **specs)
     torch.save(model, args.t)
     torch.cuda.empty_cache()
     
