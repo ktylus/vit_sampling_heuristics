56e425d263c801bd9b403d3bec3652e5f71445c5
ktylus <karaxek23@gmail.com>
2023-12-16 20:13:30 +0100

    2 methods mostly implemented

---
+++ src/color_histograms_sampler.py
@@ -0,0 +1,112 @@
+import torch
+from torchvision import datasets, transforms
+
+from elastic_vit_example.patch_sampler import GridSamplerV2
+from elastic_vit_example.custom_dataset import CustomDataset
+from utils import batch_histogram
+
+
+def find_top_k_entropy_patches(patches, k):
+    patch_entropy = calculate_patches_entropy(patches)
+    sorted_entropy, patch_indices = torch.sort(patch_entropy, descending=True, dim=-1)
+    return patch_indices[:k]
+
+
+def calculate_patches_entropy(patches):
+    histograms = batch_histogram(patches.flatten(-2) * 255, 16)
+    histograms = torch.where(histograms == 0, 1, histograms)
+    histograms = torch.nn.functional.normalize(histograms, p=1, dim=-1)
+    patches_entropy = -torch.sum((histograms * torch.log2(histograms)), dim=-1)
+    patches_entropy = torch.sum(patches_entropy, dim=-1)
+    return patches_entropy
+
+
+def divide_patch_coords_in_four(coords):
+    patch_sizes = torch.cat((
+        (coords[:, 2] - coords[:, 0]).unsqueeze(1),
+        (coords[:, 3] - coords[:, 1]).unsqueeze(1)
+    ), dim=-1)
+
+    upper_left = torch.cat((
+        coords[:, :2],
+        coords[:, 2:] - patch_sizes / 2
+    ), dim=-1)
+    upper_right = torch.cat((
+        coords[:, 0].unsqueeze(1),
+        (coords[:, 1] + patch_sizes[:, 1] / 2).unsqueeze(1),
+        (coords[:, 2] - patch_sizes[:, 0] / 2).unsqueeze(1),
+        coords[:, 3].unsqueeze(1)
+    ), dim=-1)
+    lower_left = torch.cat((
+        (coords[:, 0] + patch_sizes[:, 0] / 2).unsqueeze(1),
+        coords[:, 1].unsqueeze(1),
+        coords[:, 2].unsqueeze(1),
+        (coords[:, 3] - patch_sizes[:, 1] / 2).unsqueeze(1)
+    ), dim=-1)
+    lower_right = torch.cat((
+        coords[:, :2] + patch_sizes / 2,
+        coords[:, 2:]
+    ), dim=-1)
+
+    coords_after_division = torch.cat((
+        upper_left, upper_right,
+        lower_left, lower_right
+    ), dim=0)
+    return coords_after_division
+
+
+def divide_patches_pixels_in_four(patches):
+    patch_size = patches.shape[-1]
+    upper_left = patches[:, :, :(patch_size // 2), :(patch_size // 2)]
+    upper_right = patches[:, :, :(patch_size // 2), (patch_size // 2):]
+    lower_left = patches[:, :, (patch_size // 2):, :(patch_size // 2)]
+    lower_right = patches[:, :, (patch_size // 2):, (patch_size // 2):]
+
+    resize = transforms.Resize((patch_size, patch_size), antialias=True)
+    upper_left = resize.forward(upper_left)
+    upper_right = resize.forward(upper_right)
+    lower_left = resize.forward(lower_left)
+    lower_right = resize.forward(lower_right)
+
+    patches_after_division = torch.cat((
+        upper_left, upper_right,
+        lower_left, lower_right
+    ), dim=0)
+    return patches_after_division
+
+
+# patch_indices is [batch_size, n_patches] - indices of patches to divide
+def divide_patches_in_four(patches, coords, patch_indices):
+    coords_to_divide = torch.index_select(coords, dim=0, index=patch_indices)
+    patches_to_divide = torch.index_select(patches, dim=0, index=patch_indices)
+    divided_coords = divide_patch_coords_in_four(coords_to_divide)
+    divided_patches = divide_patches_pixels_in_four(patches_to_divide)
+
+    undivided_patches_filter = torch.ones(patches.shape[0], dtype=torch.bool)
+    undivided_patches_filter[patch_indices] = False
+    undivided_coords = coords[undivided_patches_filter]
+    undivided_patches = patches[undivided_patches_filter]
+
+    all_coords = torch.cat((undivided_coords, divided_coords), dim=0)
+    all_patches = torch.cat((undivided_patches, divided_patches), dim=0)
+    return all_patches, all_coords
+
+
+def sample_patches(image, k):
+    grid_sampler = GridSamplerV2()
+    grid_patches, grid_coords = grid_sampler(image)
+    top_entropy_patches_indices = find_top_k_entropy_patches(grid_patches, k)
+    patches, coords = divide_patches_in_four(grid_patches, grid_coords, top_entropy_patches_indices)
+    return patches, coords
+
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+flowers = datasets.Flowers102("../data/flowers", transform=transform)
+img = flowers[0][0]
+
+print(sample_patches(img, 10)[0].shape)
+print(sample_patches(img, 10)[1].shape)
